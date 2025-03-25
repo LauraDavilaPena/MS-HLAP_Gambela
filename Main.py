20b921ac3e136b813d60c1b1ac605837b2113414
ShavaraniMahdi <shavarani.mahdi@gmail.com>
2025-03-25 15:30:21 +0000

    added problem and main classes

---
+++ Main.py
@@ -0,0 +1,28 @@
+import Location_Problem as problem
+import pygmo as pg
+import time
+
+    
+# Record the start time
+start_time = time.time()
+
+pop_size =200
+gen = 200
+model_data = ...
+p = pg.problem(problem.Location_Problem(model_data))
+pop = pg.population(prob=p, size=pop_size, seed=12314876)
+algo = pg.algorithm(pg.nsga2(gen=gen))
+pop = algo.evolve(pop)
+
+
+end_time = time.time()
+
+# Calculate and print the elapsed time
+elapsed_time = end_time - start_time
+# print(f"Elapsed Time: {elapsed_time} seconds")
+
+# Save the best solution
+Best_soluton = pop.get_x()[pop.best_idx()]
+original_p = p.extract(problem.Location_Problem)
+KPIs = original_p.fitness(Best_soluton)
+open_hps, open_hcs = original_p.decode_facilities(Best_soluton)
